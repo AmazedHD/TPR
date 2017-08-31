c22c93dc005613685a72fc7cbce948be6b199744
Erwin Henraat <e.henraat@ma-web.nl>
2017-08-31 21:31:26 +0200

    added assignment to lesson2

--- les2/VariablesAndDatatypes.java
+++ les2/VariablesAndDatatypes.java
@@ -1,7 +1,4 @@
 public class VariablesAndDatatypes{
-
-
-
     public static void main(String args[]){
       String theName = "Klaas";
       int theAge = 38;
@@ -14,9 +11,18 @@ public class VariablesAndDatatypes{
       theAge += 1;
       System.out.println("I'm getting older. I am now "+ theAge);
 
-
       Person henk = new Person("Henk", 20, 76.5f);
       henk.showStats();
+
+      //Beantwoord de onderstaande vragen en voer de opdrachten uit.
+      //Als je dit gedaan hebt kun je het achievement "variables & datatypes basics" af laten tekenen:
+
+      //Wat is het gewicht van de persoon met de naam Klaas?
+      //Wat is de leeftijd van de persoon met de naam Klaas?
+      //Maak 3 variabelen myName, myAge en myWeight. Vul daar je eigen gegevens in
+      //Verhoog in een nieuwe regel code de waarde van myAge met 1
+      //Maak ook een nieuwe variabele van het datatype persoon aan, vul je eigen gegevens in bij deze persoon
+
   }
 
 }
