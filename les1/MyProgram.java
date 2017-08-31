8d1c2c1d9838fa74b0f0d3e24767c7eef7a513d5
Erwin Henraat <e.henraat@ma-web.nl>
2017-08-31 16:16:49 +0200

    example code added for lesson 1

---
+++ les1/MyProgram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class MyProgram{
+  public static void main(String args[]){
+    System.out.println("hello world");
+
+    Scanner s = new Scanner(System.in);
+    System.out.println("you just typed " + s.next());
+
+
+
+  }
+}
