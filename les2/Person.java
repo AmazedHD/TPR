6aa352038fb479517c46c63a059d7b9a11143d32
Erwin Henraat <e.henraat@ma-web.nl>
2017-09-03 13:09:26 +0200

    nog een update variablesAndDatatypes assignment

--- les2/Person.java
+++ les2/Person.java
@@ -1,12 +1,19 @@
-public class Person{
+public class Person{  //Naam van de class oftewel het nieuw datatype
+
+  //Variabelen waaruit Person is opgebouwd
   private String name;
   private int age;
   private float weight;
+
+  //Een zogenaamde "constructor". Een "functie" met de naam van de class
+  //Hiermee kan de informatie over de Person worden toegekend aan de variabelen
   public Person(String name, int age, float weight){
     this.name = name;
     this.age = age;
     this.weight = weight;
   }
+  //Nog een functie voor het datatype Person
+  //Functies worden in week 4 behandeld
   public void showStats(){
     System.out.println(name + " is " + age + " years old and weighs " + weight);
   }
--- les2/VariablesAndDatatypes.java
+++ les2/VariablesAndDatatypes.java
@@ -1,44 +1,61 @@
 public class VariablesAndDatatypes{
     public static void main(String args[]){
+      //Variabelen met verschillende datatypes
+      //Optioneel worden daar de waarden aan toegekend met behulp van = met daarachter de nieuwe waarden
+      //Let op dat je bij het toekennen van een String "" double quotes gebruikt
       String thePersonName = "Klaas";
       int thePersonAge = 38;
       float thePersonWeight = 82.4f;
 
+      //Het printen van de waarde van een variabele naar de command prompt
       System.out.println("Hello my name is "+ thePersonName);
 
-      theWeight = theWeight + 10;
+      thePersonWeight += 10; //tel 10 op bij de waarde van thePersonWeight
       System.out.println("My weight has increased to " + thePersonWeight);
 
-      theAge += 1;
+      thePersonAge += 1; //tel 1 op bij de waarde van thePersonAge
       System.out.println("I'm getting older. I am now "+ thePersonAge);
 
+      //Het aanmaken van een object van het type Person
       Person henk = new Person("Henk", 20, 76.5f);
+      //Person is een datatype dat ik zelf heb gemaakt dankzij Person.java
+
+      //het Person datatype bevat een functie showStats() die kan worden uitgevoerd
       henk.showStats();
+
+      Person piet = new Person("Piet", 55, 57.5f);
+      piet.showStats();
 
       /*
       Beantwoord de onderstaande vragen en voer de opdrachten uit.
-      Geef antwoorden op vragen door ze te printen naar de command prompt
-      Als je dit gedaan hebt kun je het achievement "variables & datatypes basics" af laten tekenen bij de docent:
+      Geef antwoorden op vragen door ze te printen naar de command prompt met System.out.println();
+      Als je dit gedaan hebt gedaan kun je het achievement "variables & datatypes basics" af laten tekenen bij de docent.
       */
 
-      //Wat is het gewicht van de persoon met de naam Klaas?
+      //Vraag 1: Wat is het gewicht van de persoon met de naam "Klaas"?
       System.out.println("je antwoord hier");
-      //Wat is de leeftijd van de persoon met de naam Klaas?
 
-      //Maak 3 variabelen myName, myAge en myWeight. Vul daar je eigen gegevens in
+      //Vraag 2: Wat is de leeftijd van de persoon met de naam "Piet"?
+      System.out.println("je antwoord hier");
 
-      //Verhoog in een nieuwe regel code de waarde van myAge met 1
+      //Opdracht 1: Maak 3 variabelen: myName, myAge en myWeight. Zorg dat ze het juiste datatype hebben en vul als waarden je eigen gegevens in.
 
-      //Maak ook een nieuwe variabele van het datatype "Person" aan, vul je eigen gegevens in bij deze persoon
+      //Opdracht 2: Verhoog met een regel code de waarde van myAge met 1
+
+      //Opdracht 3: Zoek uit wat de mogelijke waarden zijn van het datatype "boolean". Maak een variabele aan waarin je informatie over jezelf opslaat met een boolean.
+
+      //Opdracht 4: Maak een nieuwe variabele van het datatype "Person" aan, vul je eigen gegevens in bij deze persoon
 
       int mysteryType = -1.472727393d;
-      //Het bovensraande datatype klopt niet met de waarde. Als je de code probeert te compileren zul je daarom ook een error krijgen
+      //Opdracht 5:
+      //Het bovensraande datatype klopt niet met de waarde. Als je de code probeert te compileren zul je daarom ook een error krijgen.
       //Zoek uit welk datatype het getal 1.472727393d moet hebben en verander het datatype naar het juiste.
 
-      //Bedenk en leg uit waarom er een d achter het getal staat.
+      //Vraag 3: Bedenk en leg uit waarom er een d achter het getal in opdracht 5 staat.
+      System.out.println("je antwoord hier");
 
-      //Kun je ook uitleggen waarom de waarde van de variabele theWeight een f achter het getal heeft staan? leg uit.
-
+      //Vraag 4: Kun je ook uitleggen waarom de waarde van de variabele theWeight een f achter het getal heeft staan? leg uit.
+      System.out.println("je antwoord hier");
 
   }
 
