4b306020fb85070d79dd6454b73ba0e14a6f3388
Erwin Henraat <e.henraat@ma-web.nl>
2017-09-03 11:39:31 +0200

    update variablesAndDatatypes assignment

--- les2/VariablesAndDatatypes.java
+++ les2/VariablesAndDatatypes.java
@@ -1,27 +1,44 @@
 public class VariablesAndDatatypes{
     public static void main(String args[]){
-      String theName = "Klaas";
-      int theAge = 38;
-      float theWeight = 82.4f;
-      System.out.println("Hello my name is "+ theName);
+      String thePersonName = "Klaas";
+      int thePersonAge = 38;
+      float thePersonWeight = 82.4f;
+
+      System.out.println("Hello my name is "+ thePersonName);
 
       theWeight = theWeight + 10;
-      System.out.println("My weight has increased to " + theWeight);
+      System.out.println("My weight has increased to " + thePersonWeight);
 
       theAge += 1;
-      System.out.println("I'm getting older. I am now "+ theAge);
+      System.out.println("I'm getting older. I am now "+ thePersonAge);
 
       Person henk = new Person("Henk", 20, 76.5f);
       henk.showStats();
 
-      //Beantwoord de onderstaande vragen en voer de opdrachten uit.
-      //Als je dit gedaan hebt kun je het achievement "variables & datatypes basics" af laten tekenen:
+      /*
+      Beantwoord de onderstaande vragen en voer de opdrachten uit.
+      Geef antwoorden op vragen door ze te printen naar de command prompt
+      Als je dit gedaan hebt kun je het achievement "variables & datatypes basics" af laten tekenen bij de docent:
+      */
 
       //Wat is het gewicht van de persoon met de naam Klaas?
+      System.out.println("je antwoord hier");
       //Wat is de leeftijd van de persoon met de naam Klaas?
+
       //Maak 3 variabelen myName, myAge en myWeight. Vul daar je eigen gegevens in
+
       //Verhoog in een nieuwe regel code de waarde van myAge met 1
-      //Maak ook een nieuwe variabele van het datatype persoon aan, vul je eigen gegevens in bij deze persoon
+
+      //Maak ook een nieuwe variabele van het datatype "Person" aan, vul je eigen gegevens in bij deze persoon
+
+      int mysteryType = -1.472727393d;
+      //Het bovensraande datatype klopt niet met de waarde. Als je de code probeert te compileren zul je daarom ook een error krijgen
+      //Zoek uit welk datatype het getal 1.472727393d moet hebben en verander het datatype naar het juiste.
+
+      //Bedenk en leg uit waarom er een d achter het getal staat.
+
+      //Kun je ook uitleggen waarom de waarde van de variabele theWeight een f achter het getal heeft staan? leg uit.
+
 
   }
 
